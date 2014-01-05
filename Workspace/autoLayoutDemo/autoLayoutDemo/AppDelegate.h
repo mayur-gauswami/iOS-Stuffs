caeecbe1ec54775054a66e6afecd21ca93ae399f
mayur gauswami <gosaimayur91@yahoo.com>
2014-01-05 15:59:15 +0530

    added workspace

---
+++ Workspace/autoLayoutDemo/autoLayoutDemo/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  autoLayoutDemo
+//
+//  Created by Mayur Gosai on 05/01/14.
+//  Copyright (c) 2014 Mayur Gosai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MainViewController.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Workspace/autoLayoutDemo/autoLayoutDemo/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  autoLayoutDemo
+//
+//  Created by Mayur Gosai on 05/01/14.
+//  Copyright (c) 2014 Mayur Gosai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
