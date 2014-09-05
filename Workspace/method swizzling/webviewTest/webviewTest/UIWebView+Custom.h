7c6d97dc3005b7d52819caf666ff65882e2b4559
mayur gauswami <gosaimayur91@yahoo.com>
2014-09-05 15:17:26 +0530

    custome webview

--- Workspace/method swizzling/webviewTest/webviewTest/UIWebView+Custom.h
+++ Workspace/method swizzling/webviewTest/webviewTest/UIWebView+Custom.h
@@ -8,6 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
+<<<<<<< HEAD
 @interface UIWebView (Custom)
+=======
+@interface UIWebView (UIWebViewDelegate) <UIWebViewDelegate>
+
+@property (nonatomic, retain) id webViewDelegate;
+>>>>>>> b5020d83ee225fde304886b215afbdee6fd18249
 
 @end
